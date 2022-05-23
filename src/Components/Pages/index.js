9808c5517d31ad3f768cb8aa14b0637be1b64f0b
JithanRoy <jonyyy81@gmail.com>
2022-05-23 17:40:00 +0100

    first commit

---
+++ src/Components/Pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddUsers from '../AddUsers/AddUsers'
+import Users from '../Users/Users'
+
+const index = () => {
+  return (
+    <div>
+        <AddUsers/>
+        <Users/>
+    </div>
+  )
+}
+
+export default index
