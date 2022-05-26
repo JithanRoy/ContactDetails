00bc25488c07dc7e96fde3fae0baffa8aa04134f
JithanRoy <jonyyy81@gmail.com>
2022-05-26 18:01:29 +0100

    final changes button

--- src/Components/AddUsers/AddUsers.js
+++ src/Components/AddUsers/AddUsers.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const AddUsers = (props) => {
   const {setContactList} = props;
   const [inputValue, setInputValue ] = React.useState({
@@ -30,14 +29,26 @@ const AddUsers = (props) => {
 
   return (
       <div className="Inputfields w-96 h-96 m-40 rounded ">
-          <form onSubmit={handleSubmit} className='flex flex-col p-10 pl-10 bg-slate-400 rounded' >
-            <label htmlFor="nameInput">Name: </label>
-            <input className='nameInput m-5 ml-0 mt-1 p-1' type="text" name='name' placeholder='Please enter your name' onChange={addHandler}></input>
-            <label htmlFor="nameInput">Email: </label>
-            <input className='EmailInput m-5 ml-0 mt-1 p-1' type="email"name='email' placeholder='Please enter your email' onChange={addHandler}></input>
-            <label htmlFor="nameInput">Address: </label>
-            <textarea className='AddressInput m-5 ml-0 mt-1 p-1' type="text"name='address' placeholder='Please enter your address' onChange={addHandler}></textarea> 
-            <button type="submit"> Submit </button>
+          <form onSubmit={handleSubmit} className='flex flex-col p-10 bg-slate-400 rounded' >
+            <div className='m-2 flex flex-col'>
+                <label htmlFor="nameInput">Name: </label>
+                <input className='nameInput p-1 rounded' type="text" name='name' placeholder='Please enter your name' onChange={addHandler}></input>
+            </div>
+
+            <div className='m-2 flex flex-col'>
+                  <label htmlFor="nameInput">Email: </label>
+                  <input className='EmailInput p-1 rounded' type="email"name='email' placeholder='Please enter your email' onChange={addHandler}></input>
+            </div>
+
+            <div className='m-2 flex flex-col'>
+                <label htmlFor="nameInput">Address: </label>
+                <textarea className='AddressInput p-1 rounded' type="text"name='address' placeholder='Please enter your address' onChange={addHandler}></textarea> 
+            </div>
+
+            <div className='m-2 flex flex-col rounded'>
+              <button type="submit" className = "p-2 bg-sky-600 hover:bg-sky-700 ... rounded" > Submit </button>
+            </div>
+
           </form>
       </div>
   )
