0f7ea83dc16be5f5230f21d80ababe4496a40aea
JithanRoy <jonyyy81@gmail.com>
2022-06-29 14:37:47 +0100

    first commit

---
+++ src/Components/userList/UserList.js
@@ -0,0 +1,44 @@
+import { Button } from '@mui/material';
+import axios from 'axios';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Edit from '../Edit';
+import { Delete } from '../Delete';
+
+const UserList = () => {
+    const [users, setUser] = useState([]);
+
+    useEffect(()=> { 
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(response => {
+          setUser(response);
+        }); 
+    }, [])
+
+    // console.log(users);
+
+  return (
+      <div>
+        {
+          users.map((user) => (
+            <div>
+                <div>
+                    <h2>User ID : {user.id}</h2>
+                    <h4>Name :  {user.name}</h4>
+                    <p>Phone : {user.phone}</p>
+                    <p>Email : {user.email}</p>
+                </div>
+
+                <div>
+                  <button> <Edit/> </button>
+                  <button> <Delete/> </button>
+                </div>
+            </div>
+          ))
+        }
+      </div>
+  )
+}
+
+export default UserList;
